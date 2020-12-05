9e3bf4a60b30e14153cd8f1c63cbaa825e33e5ea
Christian <christian.jung@po8earr.com>
2020-12-06 00:54:35 +0100

    Add argparse for argument parsing

--- stdwebcheck.py
+++ stdwebcheck.py
@@ -22,15 +22,15 @@ def str2bool(v):
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-f','--file', type=str, help='Use a file that contains a copy of burp request',default="")
-parser.add_argument('-u','--url', type=str, help='Standard webcall to given url',default="")
-parser.add_argument('-v','--verbose', type=str2bool, nargs='?', const=True, default=False,help='Detailed output')
-parser.add_argument('-dr','--dir', type=str, help='Define the directory which is used to save all the files into. Without this option the current directory is used',default="")
-parser.add_argument('-da', type=str, help='Absolute path',default="")
+parser.add_argument('-f','--file', type=str, help='Use a file that contains a copy of burp request',default=config.file)
+parser.add_argument('-u','--url', type=str, help='Standard webcall to given url',default=config.url)
+parser.add_argument('-v','--verbose', type=str2bool, nargs='?', const=True, default=False,help='Detailed output',default=config.debug)
+parser.add_argument('-dr','--dir', type=str, help='Define the directory which is used to save all the files into. Without this option the current directory is used',default=config.path)
+parser.add_argument('-da', type=str, help='Absolute path',default=config.relative)
 
 
 soon = parser.add_argument_group('Coming soon')
-soon.add_argument('-m','--method', type=str, help='Only works with usage of url, defines the method. Default is GET', default="GET")
+soon.add_argument('-m','--method', type=str, help='Only works with usage of url, defines the method. Default is GET', default=config.method)
 soon.add_argument('-p','--port', type=str, help='Only works with usage of file, use this if the standard port is not in use (443 or 80)',default="")
 soon.add_argument('-c','--clear', type=str, help='Use this if the target is not using TLS or SSL',default="")
 soon.add_argument('--interactive', type=str, help='(Dachte dabei an soetwas wie bei jedem einzelnen Finding ja nein drücken, oder auch paramter dabei zu ändern ... ist natürlich viel Aufwand ...',default="")
