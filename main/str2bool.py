5f38f2f711c5e38a4865bc7d43f12a281cc831f5
JakeSamu <jakobnussbaumer@gmx.de>
2021-04-14 19:25:14 +0200

    debug str2bool

--- main/str2bool.py
+++ main/str2bool.py
@@ -5,9 +5,9 @@ import argparse
 def str2bool(v):
     if isinstance(v, bool):
         return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+    if v.lower() in ('yes', 'true', 't', 'y', '1', 'yy', ''):
         return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+    elif v.lower() in ('no', 'false', 'f', 'n', '0', 'nn'):
         return False
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
